98ea9ddbf24d04bad9190e2128dfe0611de53aa4
DanielRolfs <danielrolfs@icloud.com>
2022-08-19 15:58:19 +0200

    part 3

--- src/features/customerSlice.ts
+++ src/features/customerSlice.ts
@@ -1,13 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface CustomerState {
-  value: Customer[]
+  value: Customer[];
 }
 
 interface Customer {
     id: string;
     name: string;
     food: string[];
+}
+
+interface addFoodToCustomerPayload {
+    food: string;
+    id: string;
 }
 
 const initialState: CustomerState = {
@@ -21,9 +26,17 @@ export const customerSlice = createSlice({
     addCustomer: (state, action: PayloadAction<Customer>) => {
         state.value.push(action.payload);
     },
+    addFoodToCustomer: (state, action: PayloadAction<addFoodToCustomerPayload>) => {
+        state.value.forEach((customer => {
+            if(customer.id === action.payload.id) {
+                customer.food.push(action.payload.food)
+            }
+        }))
+    },
   },
 });
 
-export const { addCustomer } = customerSlice.actions
+export const addCustomer = customerSlice.actions.addCustomer
+export const addFoodToCustomer = customerSlice.actions.addFoodToCustomer
 
 export default customerSlice.reducer
